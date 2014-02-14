6d7208cd4fc4d017ad0041c2b25fb3b8b50d9db7
hoobworks <holger@hoob.net>
2014-02-14 01:06:31 +0100

    Create DeviceMessenger.h

---
+++ src/ios/DeviceMessenger.h
@@ -0,0 +1,13 @@
+//
+//  DeviceMessenger.h
+//  SignalingStorm2014
+//
+//  Created by Holger Berg on 2014-02-13.
+//
+//
+
+#import <Cordova/CDV.h>
+
+@interface DeviceMessenger : CDVPlugin
+
+@end
