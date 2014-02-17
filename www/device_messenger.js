aa023693d4f7e5de2352d5c39695faee282e8cff
hoobworks <holger@hoob.net>
2014-02-17 23:43:06 +0100

    Update device_messenger.js

--- www/device_messenger.js
+++ www/device_messenger.js
@@ -2,8 +2,8 @@ var cordovaRef = window.PhoneGap || window.cordova || window.Cordova;
 	
 var DeviceMessenger = function() {}
 
-DeviceMessenger.showPdf = function(pdfContent, success, fail) {
-	cordovaRef.exec(success, fail, 'DeviceMessenger', 'showPdf', [pdfContent]);
+DeviceMessenger.openFile = function(fileContentBase64, fileName, success, fail) {
+	cordovaRef.exec(success, fail, 'DeviceMessenger', 'openFile', [fileContentBase64, fileName]);
 }
 
 DeviceMessenger.sendArray = function(ar, success, fail) {
